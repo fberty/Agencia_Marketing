25180497cf4d562a7b4395c97d213bec850b656a
fberty <fberty09@gmail.com>
2023-01-06 22:00:30 -0300

    update

--- src/components/cases/Header.js
+++ src/components/cases/Header.js
@@ -1,45 +1,23 @@
-import { Typewriter } from 'react-simple-typewriter'
 import { Link } from 'react-router-dom'
-
+import { Typewriter } from 'react-simple-typewriter'
 
 function Header() {
+
 
 
     return (
         <main>
             <div className="relative px-6 lg:px-8">
-                <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+                <div className="mx-auto max-w-full xl:mx-12 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12 ">
                     <div>
-
                         <div>
-                            <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                                Case Studies
+                            <h1 className="text-4xl font-semibold tracking-tight pb-16 sm:text-7xl">
+                               Case Studies
                             </h1>
-                            <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                            <p className="mt-6 text-lg leading-8 text-black ">
                                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
                                 amet fugiat veniam occaecat fugiat aliqua.
                             </p>
-                            <div className="mt-8 flex gap-x-4 sm:justify-center">
-                                <a
-                                    href="#"
-                                    className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                                >
-                                    Get started
-                                    <span className="text-indigo-200" aria-hidden="true">
-                                        &rarr;
-                                    </span>
-                                </a>
-                                <a
-                                    href="#"
-                                    className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                                >
-                                    Live demo
-                                    <span className="text-gray-400" aria-hidden="true">
-                                        &rarr;
-                                    </span>
-                                </a>
-                            </div>
-
                         </div>
                         <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                             <svg
@@ -70,8 +48,9 @@ function Header() {
                         </div>
                     </div>
                 </div>
-            </div >
-        </main >
+            </div>
+        </main>
     )
 }
+
 export default Header
