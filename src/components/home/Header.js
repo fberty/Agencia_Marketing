2c71711bd0ac1bac1eda0d942a3d141b9d51571b
fberty <fberty09@gmail.com>
2023-01-09 20:56:44 -0300

    update

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -55,7 +55,7 @@ function Header() {
                             </ul>
 
                         </div>
-                        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                        <div className="absolute inset-x-0 top-[calc(100%-0rem)] -z-10 transform-gpu overflow-hidden bg-white blur-lg sm:top-[calc(100%-30rem)]">
                             <svg
                                 className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                                 viewBox="0 0 1155 678"
